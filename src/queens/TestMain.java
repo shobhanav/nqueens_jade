ced0224f06a7adcdd29684f8fabae058c35fff6c
unknown <eavnvya@SE00013598.ericsson.se>
2016-11-29 19:33:48 +0100

    Modified the test code

--- src/queens/TestMain.java
+++ src/queens/TestMain.java
@@ -10,9 +10,17 @@ import jade.wrapper.StaleProxyException;
 
 public class TestMain {
 
+	/**
+	 * SET THIS VARIABLE TO SET THE BOARD SIZE
+	 */
+	private final static int CHESS_BOARD_SIZE = 8;
+	
+	
+	
+	
 	private static TestMain instance = null;
 	private static AgentController controller;
-	private final static int num = 8;
+	
 
 	protected TestMain() throws ControllerException {
 		
@@ -21,23 +29,23 @@ public class TestMain {
 		Profile profile = new ProfileImpl();
 		jade.wrapper.AgentContainer mainContainer = rt.createMainContainer(profile);
 		
-		for(int i =0; i<num; ++i) {
+		for(int i =0; i<CHESS_BOARD_SIZE; ++i) {
 			Object[] args = new Object[2];
 			args[0] = String.valueOf(i);
-			args[1] = String.valueOf(num-1);
+			args[1] = String.valueOf(CHESS_BOARD_SIZE-1);
 			controller = mainContainer.createNewAgent("q"+i, "queens.Queen", args);
 			controller.start();
 		}
 		
 		try {
-			Thread.sleep(10000);
+			Thread.sleep(4000);
 		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		Object[] a = new Object[1];
 		
-		a[0] = String.valueOf(num);
+		a[0] = String.valueOf(CHESS_BOARD_SIZE);
 		try {
 			controller = mainContainer.createNewAgent("chess", "queens.ChessBoard", a);
 			controller.start();
@@ -58,5 +66,17 @@ public class TestMain {
 	public static void killInstance() throws StaleProxyException {
 		controller.kill();
 		instance = null;
+	}
+	
+public static void main(String[] args) throws StaleProxyException {
+		
+		TestMain controller;
+		try {
+			controller = TestMain.getInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//TestMain.killInstance();
 	}
 }
